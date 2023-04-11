b6fe6b89f0a4bbafd08fc093dc2875d74e298b92
mainz1011 <mainz1011@hotmail.com>
2023-04-11 22:01:11 +0300

    Delete products.js

--- utils/products.js
+++
@@ -1,7 +0,0 @@
-const data = {
-    products: require('../products.json').map(product => ({...product}))
-};
-const getAllProducts = () => data.products.map(product => ({...product}));
-module.exports = {
-    getAllProducts
-};
